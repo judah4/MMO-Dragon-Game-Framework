df9da297ede214cbf01583176c034bde4b6725a1
Judah Perez <judah@cookiedragon.games>
2022-06-03 22:52:21 -0700

    planning out command responses

--- Worker/UnityMmo/Assets/MmogfMessages/Health.cs
+++ Worker/UnityMmo/Assets/MmogfMessages/Health.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 
 namespace Mmogf
 {
+
+    [MessagePackObject]
+    public struct TakeDamageResponse
+    {
+        [Key(0)]
+        public bool Dead { get; set; }
+    }
+
+    [MessagePackObject]
+    public struct TakeDamageCommandRequest
+    {
+        [Key(0)]
+        public int Amount { get; set; }
+    }
+
     [MessagePackObject]
     public struct Health : IEntityComponent
     {
@@ -27,9 +42,13 @@ namespace Mmogf
             public const int CommandId = 10002;
             public int GetCommandId() => CommandId;
 
-            [Key(0)]
-            public int Amount { get; set; }
+            public TakeDamageCommandRequest Request { get; set; }
+            public TakeDamageResponse Response { get; set; }
+
+
         }
+
+        
 
         #endregion
 
