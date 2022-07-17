80ed96091f86f62039a87fe5d2ee7aa39af2ecb1
Judah Perez <judah@cookiedragon.games>
2022-07-17 14:48:52 -0700

    docker set up for servers

--- Docker/Worker/entrypoint.sh
+++ Docker/Worker/entrypoint.sh
@@ -1,2 +1,3 @@
+echo Your container args are: "$@"
 chmod +x /root/build/linuxBuild.x86_64
-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' /root/build/linuxBuild.x86_64 -batchmode -nographics
+xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' /root/build/linuxBuild.x86_64 -batchmode -nographics $@
