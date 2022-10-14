7685d2e2d08e80a9feb61925493defec4910c942
Judah Perez <judah@cookiedragon.games>
2022-10-14 08:11:29 -0700

    Planning out world grid layer

---
+++ Mmo Game Framework/Mmogf.Servers/Worlds/WorldGrid.cs
@@ -0,0 +1,43 @@
+﻿using MmoGameFramework;
+using Mmogf.Core;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mmogf.Servers.Worlds
+{
+    public class WorldGrid
+    {
+        private ConcurrentDictionary<(int x, int z), WorldCell> _cells = new ConcurrentDictionary<(int x, int z), WorldCell>();
+
+        public void AddEntity(Entity entity)
+        {
+        }
+
+        public void RemoveEntity(Entity entity)
+        {
+        }
+
+        public List<WorldCell> GetCellsInArea(Position position, float interestArea)
+        {
+
+        }
+
+        public bool WithinArea(Position point)
+        {
+            var minX = Position.X - CellSize/2f;
+            var maxX = Position.X + CellSize / 2f;
+            var minZ = Position.Z - CellSize / 2f;
+            var maxZ = Position.Z + CellSize / 2f;
+            if (point.X > maxX || point.X < minX)
+                return false;
+            if (point.Z > maxZ || point.Z < minZ)
+                return false;
+            return true;
+
+        }
+
+    }
+}
