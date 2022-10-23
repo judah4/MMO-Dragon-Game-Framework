484139c077a502a85761205ab8d859a945dcdb3c
Judah Perez <judah@cookiedragon.games>
2022-10-22 19:52:41 -0700

    Revert "move redis storage to it's own branch"
    This reverts commit c13622d8bd154b500e1abe7b68a805d00b744bc3.

---
+++ Mmo Game Framework/Mmogf.Servers/Storage/RedisStorage.cs
@@ -0,0 +1,31 @@
+﻿using ServiceStack.Caching;
+using ServiceStack.Redis;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mmogf.Servers.Storage
+{
+    public class RedisStorage
+    {
+        ICacheClient _redis;
+
+        public async Task ConnectTest()
+        {
+            if(_redis == null)
+            {
+                _redis = new MemoryCacheClient();
+            }
+            
+            var result = _redis.Add<string>("Foo", "bar");
+            Console.WriteLine(result);
+            //container.Register<IRedisClientsManager>(c =>new RedisManagerPool("gateway.docker.internal:6379"));
+            //container.Register(c => c.Resolve<IRedisClientsManager>().GetCacheClient());
+        }
+
+    }
+
+}
