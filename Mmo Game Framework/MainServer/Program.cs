b195d3bbbfa8a9b763561ef5d8e6e20db503e88f
Judah Perez <judah@cookiedragon.games>
2022-10-27 22:23:56 -0700

    Clean up some using statements

--- Mmo Game Framework/MainServer/Program.cs
+++ Mmo Game Framework/MainServer/Program.cs
@@ -13,8 +13,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Resources;
 using System.Threading;
 using System.Threading.Tasks;
 
