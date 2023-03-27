a333db31f36b81a8b77b073a2faac7992659d6e7
Judah Perez <me@judahperez.com>
2023-03-26 20:02:34 -0700

    Basic Entity Id

---
+++ Worker/UnityMmo/Packages/games.cookiedragon.mmogf.core/MmogfMessages/Core/EntityId.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace Mmogf.Core
+{
+
+    public struct EntityId
+    {
+
+        public EntityId(int id)
+        {
+           Id = id;
+        }
+
+        public int Id { get; }
+
+        public bool IsValid()
+        {
+            return Id != 0;
+        }
+
+        public static bool operator ==(EntityId a, EntityId b) => a.Equals(b);
+        public static bool operator !=(EntityId a, EntityId b) => !a.Equals(b);
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            return obj is EntityId other && Equals(other);
+        }
+
+        public bool Equals(EntityId other)
+        {
+            return Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Id.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}";
+        }
+
+    }
+}
