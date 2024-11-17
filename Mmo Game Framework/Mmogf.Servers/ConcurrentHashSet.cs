ff8004cdd097c3c923c0a60ff9521d2f6fde545b
Judah Perez <me@judahperez.com>
2024-11-16 16:19:52 -0800

    Basic concurrent hashset made

---
+++ Mmo Game Framework/Mmogf.Servers/ConcurrentHashSet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Mmogf.Servers
+{
+    public class ConcurrentHashSet<T>
+    {
+        private ConcurrentDictionary<T, byte> _internalDictionary;
+
+        public ConcurrentHashSet()
+        {
+            _internalDictionary = new ConcurrentDictionary<T, byte>();
+        }
+
+        public bool TryAdd(T item)
+        {
+            return _internalDictionary.TryAdd(item, 1);
+        }
+
+        public bool TryRemove(T item)
+        {
+            return _internalDictionary.TryRemove(item, out _);
+        }
+
+        public bool Contains(T item)
+        {
+            return _internalDictionary.ContainsKey(item);
+        }
+
+        public ICollection<T> AsCollection()
+        {
+            return _internalDictionary.Keys;
+        }
+    }
+}
